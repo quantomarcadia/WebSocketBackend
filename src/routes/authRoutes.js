a97a022748cecfb9e3e4f6ae834233bab6df4470
Badal sah <badal.shah@quantumarcadia.com>
2026-02-27 13:35:29 +0530

    added middlewares

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { verifyToken } from '../middleware/authMiddleware.js';
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+
+authRouter.get("/me", verifyToken, (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username
+  });
+});
 
 export default authRouter;
